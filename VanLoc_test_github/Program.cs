3adadb8df9e63c3ae62dc567c22633951b1df28b
Vanlocc <locnvgcd18350@fpt.edu.vn>
2020-03-23 15:40:10 +0700

    first commit

---
+++ VanLoc_test_github/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace VanLoc_test_github
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			Console.WriteLine("test github")
+		}
+	}
+}
